bbd43c961fd6cae58a47d8306ba96c2441d0e4ba
Amas9 <Amas9nine@mail.ru>
2021-12-13 22:36:15 +0600

    *2-two*

--- main.js
+++ main.js
@@ -1,3 +1,12 @@
 function name(params) {
     let main = `main`
 }
+
+let a = 500
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+console.log(life)
--- second.js
+++ second.js
@@ -2,3 +2,11 @@ function name(params) {
 let second = `second`
 }
 
+let a = 500
+
+for (let index = 0; index < array.length; index++) {
+    const element = array[index];
+    
+}
+
+console.log(life)
